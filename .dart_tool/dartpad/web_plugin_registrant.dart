15822a73b74888c60e2eea3af8e7710a96b73901
YumNumm <onory0917@gmail.com>
2023-07-30 02:00:41 +0900

    add: riverpod

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,16 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_performance_web/firebase_performance_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebasePerformanceWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
