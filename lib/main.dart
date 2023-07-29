5177b67716a550717ede4741e8d3e4237b2fa5e0
YumNumm <onory0917@gmail.com>
2023-07-30 01:07:56 +0900

    fix: gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
-Future<void> main() async {}
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
+
+
+}
